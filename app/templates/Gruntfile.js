8e4d7685c22e1da435c8305689f044008ee5e48e
Ain Tohvri <at@interactive-pioneers.de>
2014-10-30 16:50:01 +0100

    Fix font copying to distribution

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -387,7 +387,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'fonts/*.{eot,svg,ttf,woff}'
           ]
         }, {
           src: 'node_modules/apache-server-configs/dist/.htaccess',
