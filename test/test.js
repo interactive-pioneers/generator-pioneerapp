08ea358aea9dea0e5e64624eef225d340a729b92
Ain Tohvri <at@interactive-pioneers.de>
2016-07-11 13:28:44 +0200

    Fix legacy Yeoman test and assert requires

--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@
 
 var path = require('path');
 var assert = require('assert');
-var helpers = require('yeoman-generator').test;
-var assert = require('yeoman-generator').assert;
+var helpers = require('yeoman-test');
+var assert = require('yeoman-assert');
 var _ = require('underscore');
 
 describe('Webapp generator', function() {
