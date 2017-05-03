98903532cf8521797f767349cd53a626c139664c
Ain Tohvri <at@interactive-pioneers.de>
2017-05-03 13:41:36 +0200

    Add additional Modernizr specs for better coverage

--- test/modernizr.js
+++ test/modernizr.js
@@ -4,19 +4,62 @@ var helpers = require('yeoman-test');
 var assert = require('yeoman-assert');
 
 describe('Modernizr feature', function() {
+
+
   describe('on', function() {
-    before(function(done) {
-      helpers.run(path.join(__dirname, '../app'))
-        .withArguments(['webapp'])
-        .withPrompts({features: [
-          'includeModernizr'
-        ]})
-        .on('end', done);
+    context('without Assemble', function() {
+      before(function(done) {
+        helpers.run(path.join(__dirname, '../app'))
+          .withArguments(['webapp'])
+          .withPrompts({features: [
+            'includeModernizr'
+          ]})
+          .on('end', done);
+      });
+
+      it('should add dependencies', function() {
+        assert.fileContent('package.json', '"grunt-modernizr"');
+        assert.fileContent('bower.json', '"modernizr"');
+      });
+
+      it('should add script tag', function() {
+        assert.fileContent('app/index.html', 'modernizr.js');
+      });
+
+      it('should add no-js class', function() {
+        assert.fileContent('app/index.html', 'class="no-js"');
+      });
     });
 
-    it('should add dependencies', function() {
-      assert.fileContent('package.json', '"grunt-modernizr"');
-      assert.fileContent('bower.json', '"modernizr"');
+    context('with Assemble', function() {
+      before(function(done) {
+        helpers.run(path.join(__dirname, '../app'))
+          .withArguments(['webapp'])
+          .withPrompts({features: [
+            'includeModernizr',
+            'includeAssemble'
+          ]})
+          .on('end', done);
+      });
+
+      it('should add dependencies', function() {
+        assert.fileContent('package.json', '"grunt-modernizr"');
+        assert.fileContent('bower.json', '"modernizr"');
+      });
+
+      it('should add script tag', function() {
+        assert.fileContent([
+          ['app/index.html', 'modernizr.js'],
+          ['src/templates/layouts/default.hbs', 'modernizr.js']
+        ]);
+      });
+
+      it('should add no-js class', function() {
+        assert.fileContent([
+          ['app/index.html', 'class="no-js"'],
+          ['src/templates/layouts/default.hbs', 'class="no-js"']
+        ]);
+      });
     });
   });
 
